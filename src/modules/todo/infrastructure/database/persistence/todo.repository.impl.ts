365ac938b44777d57fecd9211b879134258f9fdb
JoaoPedroS <j.pedrosantos009@gmail.com>
2026-02-11 11:42:13 -0300

    fix: retiradas desabilitações de eslint desnecessárias

--- src/modules/todo/infrastructure/database/persistence/todo.repository.impl.ts
+++ src/modules/todo/infrastructure/database/persistence/todo.repository.impl.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { DataSource, EntityManager, Repository } from 'typeorm';
 import {
   PaginatedResult,
